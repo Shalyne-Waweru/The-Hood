2e15b3a43cf86ca33a87c0bf72479c1d4f724277
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-20 10:41:46 +0300

    updating the app forms

--- hood/forms.py
+++ hood/forms.py
@@ -13,10 +13,21 @@ class SignupForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
 
+# class UpdateProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         exclude = ('user', 'neighbourhood')
+
+class UpdateUserInfoForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ('user', 'neighbourhood')
+        fields = ['profile_image', 'bio','location', 'neighbourhood']        
 
 
 class NeighbourHoodForm(forms.ModelForm):
