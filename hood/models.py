f4116484667ceca0eb5a9e97c0933816c655e7f0
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-20 23:19:25 +0300

    updating index

--- hood/models.py
+++ hood/models.py
@@ -38,6 +38,8 @@ class Profile(models.Model):
      
     def __str__(self):
             return f'{self.user.username} profile'
+        
+    
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
